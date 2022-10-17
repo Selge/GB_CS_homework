04b43d7bca101a5f0fc512a210a40eef78b9faf3
Sergei Shcherbak <sshch1987@gmail.com>
2022-10-17 22:39:24 +0200

    Create Homework_4.cs

---
+++ Homework_4.cs
@@ -0,0 +1,107 @@
+﻿Console.WriteLine("GB C# Homework. Stage 4.");
+
+int Number()
+{
+    Console.WriteLine("Please type in your target number: ");
+    var income = Console.ReadLine();
+    int N = Convert.ToInt32(income);
+    return N;
+}
+
+Console.WriteLine("GB C# Homework #4: Task_1 ('Задача 25').");
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+void MathDegree()
+{
+    var A = Number();
+    var B = Number();
+    int number = 1;
+    for (int i=1;i<=B;i++) 
+    {
+        number *= A;   
+    }
+    string f_result = string.Format("{0:# ### ###}", number);
+    Console.WriteLine($"Your target number is: {A}.");
+    Console.WriteLine($"Your degree number is: {B}.");
+    Console.WriteLine($"{A}^{B} = {f_result}");
+}
+
+MathDegree();
+
+
+Console.WriteLine("GB C# Homework #4: Task_2 ('Задача 27').");
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+void Summary()
+{
+    var number = Number();
+    Console.WriteLine($"Your start number is: {number}");
+    int summ = 0;
+    while (number > 0)
+    {
+        summ += number%10;
+        number /= 10;
+    }
+    Console.WriteLine($"Your final number is: {summ}");
+}
+
+Summary();
+
+
+Console.WriteLine("GB C# Homework #4: Task_3 ('Задача 29').");
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+// 6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+int [] Massive()
+{
+    Console.WriteLine("Please, choose desired array formation type.");
+    Console.WriteLine("Please, type in 'a' to create an array automatically or 'm' to put in the numbers manually:");
+    var income = Convert.ToString(Console.ReadLine());
+    Console.WriteLine($"You have chosen {income}");
+
+    if(income == "a")
+    {
+        Console.WriteLine("Please, type in desired array length:");
+        var t_length = Console.ReadLine();
+        var c_length = Convert.ToInt32(t_length);
+
+        int [] mass = new int[c_length];
+        Random chisla = new Random();
+        for (int i = 0; i < mass.Length; i++)
+        {
+            mass[i] = chisla.Next(-100, 100);
+        }
+        return mass;
+    }
+
+    else if(income == "m")
+    {
+        Console.WriteLine("Please, type in desired length of array:");
+        int new_length = Convert.ToInt32(Console.ReadLine());
+        // int new_length = int.Parse(Console.ReadLine());
+        int [] mass = new int[new_length];
+        for (int i = 0; i < mass.Length; i++)
+        {
+            Console.WriteLine($"Please, type in a {i} number of your array:");
+            mass[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return mass;
+    }
+
+    else
+    {
+        Console.WriteLine("Please, select from the two options provided!");
+        return Massive();
+    }  
+}
+
+void Print(int[] mass)
+{
+    int [] income = mass;
+    Console.WriteLine($"Here's your array: ({string.Join(",", income)}).");
+}
+
+Print(Massive());
