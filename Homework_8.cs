42641dc8c1909feb57a814d2e685233e965383d9
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-14 20:15:22 +0200

    Homework_8 looks finished

--- Homework_8.cs
+++ Homework_8.cs
@@ -25,19 +25,19 @@ namespace Homework_8
             switch (task)
             {
                 case 1:
-                    Aufgabe.Aufgabe54();
+                    Aufgaben.Aufgabe54();
                     break;
                 case 2:
-                    Aufgabe.Aufgabe56();
+                    Aufgaben.Aufgabe56();
                     break;
                 case 3:
-                    Aufgabe.Aufgabe58();
+                    Aufgaben.Aufgabe58();
                     break;
                 case 4:
-                    Aufgabe.Aufgabe60();
+                    Aufgaben.Aufgabe60();
                     break;
                 case 5:
-                    Aufgabe.Aufgabe62();
+                    Aufgaben.Aufgabe62();
                     break;
                 default:
                     Console.WriteLine("Please, select from the options provided!");
@@ -47,7 +47,7 @@ namespace Homework_8
         }
     }
 
-    class Aufgabe
+    class Aufgaben
     {
         public static void Aufgabe54()
         {
@@ -61,7 +61,7 @@ namespace Homework_8
         // 7 4 2 1
         // 9 5 3 2
         // 8 4 4 2
-            int[,] arr = Numbers.ArrayFillerSelector();
+            int[,] arr = NumberFillers.RealAutomaticFiller();
             Console.WriteLine("Your start array: ");
             Specials.PrintArr(arr);
             for (int i = 0; i < arr.GetLength(0); i++)
@@ -93,7 +93,7 @@ namespace Homework_8
         // 8 4 2 4
         // 5 2 6 7
         // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-            int[,] arr = Numbers.ArrayFillerSelector();
+            int[,] arr = NumberFillers.RealAutomaticFiller();
             Console.WriteLine("Your array: ");
             Specials.PrintArr(arr);
 
@@ -124,8 +124,30 @@ namespace Homework_8
         // Результирующая матрица будет:
         // 18 20
         // 15 18
-            int[,] arr_1 = Numbers.ArrayFillerSelector();
-            int[,] arr_2 = Numbers.ArrayFillerSelector();
+            var tuple = NumberFillers.PayForOneTakeTwo();
+            int[,] arr_1 = tuple.Item1;
+            int[,] arr_2 = tuple.Item2;
+            int[,] arr_Summ = new int [arr_1.GetLength(0), arr_2.GetLength(1)];
+
+            for (int i = 0; i < arr_Summ.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr_Summ.GetLength(1); j++)
+                {
+                int sum = 0;
+                for (int k = 0; k < arr_1.GetLength(1); k++)
+                {
+                    sum += arr_1[i,k] * arr_2[k,j];
+                }
+                arr_Summ[i,j] = sum;
+                }
+            }
+
+            Console.WriteLine("Your first array is: ");
+            Specials.PrintArr(arr_1);
+            Console.WriteLine("Your second array is: ");
+            Specials.PrintArr(arr_2);
+            Console.WriteLine("Array multiplication result: ");
+            Specials.PrintArr(arr_Summ);
         }
 
         public static void Aufgabe60()
@@ -138,6 +160,22 @@ namespace Homework_8
         // 34(1,0,0) 41(1,1,0)
         // 27(0,0,1) 90(0,1,1)
         // 26(1,0,1) 55(1,1,1)
+            int [,,] dreifach = NumberFillers.TripleFiller();
+
+            Console.WriteLine("Resulting array:");
+
+            for (int a = 0; a < dreifach.GetLength(0); a++)
+            {
+                for (int b = 0; b < dreifach.GetLength(1); b++)
+                {
+                    for (int c = 0; c < dreifach.GetLength(2); c++)
+                    {
+                        Console.Write( $"{dreifach[a,b,c]}({a},{b},{c}); ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
         }
 
         public static void Aufgabe62()
@@ -149,31 +187,17 @@ namespace Homework_8
         // 12 13 14 05
         // 11 16 15 06
         // 10 09 08 07
+            int[,] doppelt_vier = NumberFillers.Vier();
+            Console.WriteLine("The resulting array is: ");
+            Specials.PrintArr(doppelt_vier);
         }
     }
 
-    class Numbers
+    class NumberFillers  // Методы создания массивов. Для упрощения задачи возможность ввода массивов пользователем отключена
     {
-        public static int[,] ArrayFillerSelector()  // Меню выбора режима ввода массивов - ручного или автоматического
+        public static int[,] RealAutomaticFiller()  // Этот робот заполняет массив целыми натуральными числами.
         {
-            Console.WriteLine(@"We're kindly asking you to choose desired array formation type.
-            Please, type in 'r' to open an robot array filler ('RAF') or 'm' to use manual ('MAF') shell:");
-            var income = Convert.ToString(Console.ReadLine());
-            switch (income)
-            {
-                case "r":
-                    return RealAutomaticFiller(); // Оболочка робота для автоматического заполнения массивов
-                case "m":
-                    return ManualRealFiller(); // Оболочка заполнения массивов вручную
-                default:
-                    Console.WriteLine("Please, select from the options provided!");
-                    return ArrayFillerSelector();
-            }
-        }
-
-        public static int[,] RealAutomaticFiller()  // Этот робот заполняет массив натуральными числами.
-        {
-            Console.WriteLine("Welcome to the Automatic Filler Robot 2.0.");
+            Console.WriteLine("Welcome to the Automatic Filler Robot 2.1.");
             Console.WriteLine(@"This RAF Robot provides you with a double array of random numbers.
             Please, set the number of rows in the array:");
 
@@ -187,38 +211,101 @@ namespace Homework_8
                 {
                     for (int e = 0; e < c_columns; e++)
                     {
-                    rows_cols[i, e] = nummern.Next(11, 100); 
+                        rows_cols[i, e] = nummern.Next(11, 100); 
                     }
                 }
             return rows_cols;
         }
 
-        public static int[,] ManualRealFiller()  // Вручную заполним массив натуральными числами
+        public static (int[,], int [,]) PayForOneTakeTwo()  // Этот робот создаёт два равных массива целых натуральных чисел.
         {
-            Console.WriteLine(@"Welcome to the Manual array filler ('MAF') shell!
-            Here you may manually set an array you want.
-            Please, set the number of rows in the array: ");
-            var rows = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please, set the number of columns in the array: ");
-            var columns = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Now please set up an array of {rows} rows and {columns} columns: ");
-            int [,] rows_cols = new int [rows, columns];
-            for (int i = 0; i < rows; i++)
+            Console.WriteLine("Welcome to the Automatic Filler Robot 2.2.");
+            Console.WriteLine(@"This RAF Robot provides you with a couples of arrays of random numbers.
+            Please, set the number of rows in the arrays:");
+
+            int c_rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, set the number of columns in the arrays:");
+            int c_columns = Convert.ToInt32(Console.ReadLine());
+
+            int [,] rows_cols_1 = new int [c_rows, c_columns];
+            Random nummern_eins = new Random();
+            for (int i = 0; i < c_rows; i++)
                 {
-                    for (int e = 0; e < columns; e++)
+                    for (int e = 0; e < c_columns; e++)
                     {
-                        rows_cols[i, e] = Convert.ToInt32(Console.ReadLine());
+                        rows_cols_1 [i, e] = nummern_eins.Next(1, 10); 
                     }
                 }
+
+            int [,] rows_cols_2 = new int [c_rows, c_columns];
+            Random nummern_zwei = new Random();
+            for (int i = 0; i < c_rows; i++)
+                {
+                    for (int e = 0; e < c_columns; e++)
+                    {
+                        rows_cols_2 [i, e] = nummern_zwei.Next(1, 10); 
+                    }
+                }
+
+            return (rows_cols_1, rows_cols_2);
+        }
+
+        public static int[,,] TripleFiller() // Этот робот создаёт трехмерный массив целых натуральных чисел.
+        {
+            Console.WriteLine("Welcome to the Automatic Filler Robot 2.3.");
+            Console.WriteLine(@"This RAF Robot provides you with a 3-d array of random numbers.
+            Please, set the X measurement: ");
+            int messung_x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, set the Y measurement: ");
+            int messung_y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, set the Z measurement: ");
+            int messung_z = Convert.ToInt32(Console.ReadLine());
+        
+            int [,,] dreifach = new int[messung_x, messung_y, messung_z];
+            Random nummern_drei = new Random();
+
+            for (int i = 0; i < messung_x; i++)
+            {
+                for (int j = 0; j < messung_y; j++)
+                {
+                    for (int k = 0; k < messung_z; k++)
+                    {
+                        dreifach[i, j, k] = nummern_drei.Next(11, 99);
+                    }
+                }
+            }
+            return dreifach;
+        }
+
+        public static int[,] Vier()  // Этот робот заполняет массив 4 на 4 целыми натуральными числами.
+        {
+            int c_rows = 4;
+            int c_columns = 4;
+
+            int [,] rows_cols = new int [c_rows, c_columns];
+ 
+            int a = 1;
+            int b = 0;
+            int c = 0;
+            
+            while (a <= rows_cols.GetLength(0) * rows_cols.GetLength(1))
+            {
+                rows_cols[b, c] = a;
+                a++;
+                if (b <= c + 1 && b + c < rows_cols.GetLength(1) - 1) c++;
+                else if (b < c && b + c >= rows_cols.GetLength(0) - 1) b++;
+                else if (b >= c && b + c > rows_cols.GetLength(1) - 1) c--;
+                else b--;
+            }
             return rows_cols;
         }
     }
 
     class Specials
     {
+        // Служебный метод для печати массивов
         public static void PrintArr(int [,] rows_cols)
         {
-            Console.WriteLine("Your array is: ");
             int [,] matr = rows_cols;
             for (int i = 0; i < matr.GetLength(0); i++)
             {
