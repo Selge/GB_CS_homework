5750dd286ecf3e26babcc8ae5311ac5329a0a5c4
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-29 22:34:15 +0200

    Brushing fur))

--- Homework_1.cs
+++ Homework_1.cs
@@ -1,91 +1,151 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-Console.WriteLine("Homework_1. Task_1.");
-Console.Write ("Please enter number A: ");
-var a = Console.ReadLine();
-Console.Write ("Please enter number B: ");
-var b = Console.ReadLine();
-int num_a = Convert.ToInt32(a);
-int num_b = Convert.ToInt32(b);
-int max_1 = 0;
-if(num_a > num_b)
+﻿namespace Homework_1
 {
-    max_1 = num_a;
-}
-else{
-    max_1 = num_b;
-}
-Console.WriteLine($"Max number is {max_1}");
-
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
-Console.WriteLine("Homework_1. Task_2. Type_1.");
-Console.Write ("Please enter number D: ");
-var d = Console.ReadLine();
-Console.Write ("Please enter number E: ");
-var e = Console.ReadLine();
-Console.Write ("Please enter number F: ");
-var f = Console.ReadLine();
-int num_d = Convert.ToInt32(d);
-int num_e = Convert.ToInt32(e);
-int num_f = Convert.ToInt32(f);
-int max_2 = num_e > num_d ? num_e : num_d; // понимаю, что использование тернарного оператора скорее всего не ожидалось (не совсем по программе),
-max_2 = max_2 > num_f ? max_2 : num_f; // но мне же нужно учиться :)
-Console.WriteLine($"Max number is {max_2}");
-
-Console.WriteLine("Homework_1. Task_2. Type_2.");
-Console.Write ("Please enter number D: ");
-var d1 = Console.ReadLine();
-Console.Write ("Please enter number E: ");
-var e1 = Console.ReadLine();
-Console.Write ("Please enter number F: ");
-var f1 = Console.ReadLine();
-int num_d1 = Convert.ToInt32(d1);
-int num_e1 = Convert.ToInt32(e1);
-int num_f1 = Convert.ToInt32(f1);
-int [] array_1 = {num_d1, num_e1, num_f1};
-int max_21 = array_1.Max();
-Console.WriteLine($"Max number is {max_21}");
-
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
-Console.WriteLine("Homework_1. Task_3.");
-Console.Write ("Please enter number AB: ");
-var ab = Console.ReadLine();
-int num_ab = Convert.ToInt32(ab);
-if(num_ab % 2 == 0)
-{
-    Console.WriteLine($"{ab} is an even number");
-}
-else
-{
-    Console.WriteLine($"{ab} is an odd number");
-}
-
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
-Console.WriteLine("Homework_1. Task_4.");
-Console.Write ("Please enter number N: ");
-var N = Console.ReadLine();
-int num_N = Convert.ToInt32(N);
-int start = 1;
-var list = new int[] {};
-while(start <= num_N)
-{
-    if(start % 2 == 0)
+    class App
     {
-        Console.Write("{0} ", start);
-        start++;
+        static void Main()
+        {
+            Start.TaskSelector();
+        }
     }
-    else{
-        start++;
+
+    class Start
+    {
+        public static void TaskSelector()
+        {
+            Console.WriteLine("GB C# Homework. Stage 1.");
+            Console.WriteLine(@"Please, select the task  you're interested in from the list below:
+            - 1.Task_1 ('Задача 2').
+            - 2.Task_2 ('Задача 4').
+            - 3.Task_3 ('Задача 6').
+            - 4.Task_4 ('Задача 8').");
+            
+            Console.WriteLine("Please, type in a target Task number: ");
+            int task = Convert.ToInt32(Console.ReadLine());
+            switch (task)
+            {
+                case 1:
+                    Aufgaben.Aufgabe2();
+                    break;
+                case 2:
+                    Aufgaben.Aufgabe4();
+                    break;
+                case 3:
+                    Aufgaben.Aufgabe6();
+                    break;
+                case 4:
+                    Aufgaben.Aufgabe8();
+                    break;
+                default:
+                    Console.WriteLine("Please, select from the options provided!");
+                    TaskSelector();
+                    break;
+            }
+        }
+    }
+
+    class Aufgaben
+    {
+        public static void Aufgabe2()
+        {
+            // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+            // a = 5; b = 7 -> max = 7
+            // a = 2 b = 10 -> max = 10
+            // a = -9 b = -3 -> max = -3
+            Console.WriteLine("Homework_1. Task_1.");
+            Console.Write ("Please enter number A: ");
+            var a = Console.ReadLine();
+            Console.Write ("Please enter number B: ");
+            var b = Console.ReadLine();
+            int num_a = Convert.ToInt32(a);
+            int num_b = Convert.ToInt32(b);
+            int max_1 = 0;
+            if(num_a > num_b)
+            {
+                max_1 = num_a;
+            }
+            else{
+                max_1 = num_b;
+            }
+            Console.WriteLine($"Max number is {max_1}");
+        }
+
+        public static void Aufgabe4()
+        {
+            // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+            // 2, 3, 7 -> 7
+            // 44 5 78 -> 78
+            // 22 3 9 -> 22
+            Console.WriteLine("Homework_1. Task_2. Type_1.");
+            Console.Write ("Please enter number D: ");
+            var d = Console.ReadLine();
+            Console.Write ("Please enter number E: ");
+            var e = Console.ReadLine();
+            Console.Write ("Please enter number F: ");
+            var f = Console.ReadLine();
+            int num_d = Convert.ToInt32(d);
+            int num_e = Convert.ToInt32(e);
+            int num_f = Convert.ToInt32(f);
+            int max_2 = num_e > num_d ? num_e : num_d; // использование тернарного оператора
+            max_2 = max_2 > num_f ? max_2 : num_f; 
+            Console.WriteLine($"Max number is {max_2}");
+
+            Console.WriteLine("Homework_1. Task_2. Type_2.");
+            Console.Write ("Please enter number D: ");
+            var d1 = Console.ReadLine();
+            Console.Write ("Please enter number E: ");
+            var e1 = Console.ReadLine();
+            Console.Write ("Please enter number F: ");
+            var f1 = Console.ReadLine();
+            int num_d1 = Convert.ToInt32(d1);
+            int num_e1 = Convert.ToInt32(e1);
+            int num_f1 = Convert.ToInt32(f1);
+            int [] array_1 = {num_d1, num_e1, num_f1};
+            int max_21 = array_1.Max();
+            Console.WriteLine($"Max number is {max_21}");
+        }
+
+        public static void Aufgabe6()
+        {
+            // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+            // 4 -> да
+            // -3 -> нет
+            // 7 -> нет
+            Console.WriteLine("Homework_1. Task_3.");
+            Console.Write ("Please enter number AB: ");
+            var ab = Console.ReadLine();
+            int num_ab = Convert.ToInt32(ab);
+            if(num_ab % 2 == 0)
+            {
+                Console.WriteLine($"{ab} is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"{ab} is an odd number");
+            }
+        }
+
+        public static void Aufgabe8()
+        {
+            // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+            // 5 -> 2, 4
+            // 8 -> 2, 4, 6, 8
+            Console.WriteLine("Homework_1. Task_4.");
+            Console.Write ("Please enter number N: ");
+            var N = Console.ReadLine();
+            int num_N = Convert.ToInt32(N);
+            int start = 1;
+            var list = new int[] {};
+            while(start <= num_N)
+            {
+                if(start % 2 == 0)
+                {
+                    Console.Write("{0} ", start);
+                    start++;
+                }
+                else{
+                    start++;
+                }
+            }
+        }
     }
 }
--- Homework_2.cs
+++ Homework_2.cs
@@ -1,67 +1,120 @@
-﻿Console.WriteLine("GB C# Homework Part_2");
-
-// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-Console.WriteLine("GB_C#_Homework_2 Task_1");
-Console.WriteLine("Please enter a 3-digit number (number A): ");
-Start_A:
-var numberA = Console.ReadLine();
-var digitArrayA = numberA!.ToArray();
-if(digitArrayA.Length >= 3)
+﻿namespace Homework_2
 {
-    Console.WriteLine($"The second digit of {numberA} is {digitArrayA[1]}");
-}
-else
-{
-    Console.WriteLine($"{numberA} is not sufficient. Please enter a 3-digit number:");
-    goto Start_A;
-}
-
-
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-Console.WriteLine("GB_C#_Homework_2 Task_2");
-Console.WriteLine("Please enter a number B: ");
-var numberB = Console.ReadLine();
-var digitArrayB = numberB!.ToArray();
-if(digitArrayB.Length >= 3)
-{
-    Console.WriteLine($"The third digit of {numberB} is {digitArrayB[2]}");
-}
-else
-{
-    Console.WriteLine($"{numberB} has no 3-rd digit");
-}
-
-// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-Console.WriteLine("GB_C#_Homework_2 Task_3");
-Console.WriteLine("Please enter a day of the week number (from 1 to 7): ");
-Start_B:
-var day = Console.ReadLine();
-int week_num = Convert.ToInt32(day);
-string [] wochentage = new string [] {"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"};
-if(week_num > 7)
-{
-    Console.WriteLine($"{day} is not a 'day of the week' number.");
-    Console.WriteLine("Please, type in another value:");
-    goto Start_B;
-}
-else
-{
-    // Здесь не совсем магическое число: в классической 7-ми дневной неделе как правило 2 выходных дня. Поэтому отнимаем от общей длины массива два дня.
-    if(week_num <= 5)
+    class App
     {
-        Console.Write($"{wochentage[week_num - 1]}({day}) in not a weekend day, it's a business day. Go back to work!");
+        static void Main()
+        {
+            Start.TaskSelector();
+        }
     }
-    else
+
+    class Start
     {
-        Console.Write($"{wochentage[week_num - 1]}({day}) is a weekend day. Have some rest.");
+        public static void TaskSelector()
+        {
+            Console.WriteLine("GB C# Homework. Stage 2.");
+            Console.WriteLine(@"Please, select the task  you're interested in from the list below:
+            - 1.Task_1 ('Задача 10').
+            - 2.Task_2 ('Задача 13').
+            - 3.Task_3 ('Задача 15').");
+            
+            Console.WriteLine("Please, type in a target Task number: ");
+            int task = Convert.ToInt32(Console.ReadLine());
+            switch (task)
+            {
+                case 1:
+                    Aufgaben.Aufgabe10();
+                    break;
+                case 2:
+                    Aufgaben.Aufgabe13();
+                    break;
+                case 3:
+                    Aufgaben.Aufgabe15();
+                    break;
+                default:
+                    Console.WriteLine("Please, select from the options provided!");
+                    TaskSelector();
+                    break;
+            }
+        }
+    }
+
+    class Aufgaben
+    {
+        public static void Aufgabe10()
+        {
+            Console.WriteLine("GB C# Homework Part_2");
+
+            // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            // 456 -> 5
+            // 782 -> 8
+            // 918 -> 1
+            Console.WriteLine("GB_C#_Homework_2 Task_1");
+            Console.WriteLine("Please enter a 3-digit number (number A): ");
+            Start_A:
+            var numberA = Console.ReadLine();
+            var digitArrayA = numberA!.ToArray();
+            if(digitArrayA.Length >= 3)
+            {
+                Console.WriteLine($"The second digit of {numberA} is {digitArrayA[1]}");
+            }
+            else
+            {
+                Console.WriteLine($"{numberA} is not sufficient. Please enter a 3-digit number:");
+                goto Start_A;
+            }
+        }
+
+        public static void Aufgabe13()
+        {
+            // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+            // 645 -> 5
+            // 78 -> третьей цифры нет
+            // 32679 -> 6
+            Console.WriteLine("GB_C#_Homework_2 Task_2");
+            Console.WriteLine("Please enter a number B: ");
+            var numberB = Console.ReadLine();
+            var digitArrayB = numberB!.ToArray();
+            if(digitArrayB.Length >= 3)
+            {
+                Console.WriteLine($"The third digit of {numberB} is {digitArrayB[2]}");
+            }
+            else
+            {
+                Console.WriteLine($"{numberB} has no 3-rd digit");
+            }
+        }
+
+        public static void Aufgabe15()
+        {
+            // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+            // 6 -> да
+            // 7 -> да
+            // 1 -> нет
+            Console.WriteLine("GB_C#_Homework_2 Task_3");
+            Console.WriteLine("Please enter a day of the week number (from 1 to 7): ");
+            Start_B:
+            var day = Console.ReadLine();
+            int week_num = Convert.ToInt32(day);
+            string [] wochentage = new string [] {"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"};
+            if(week_num > 7)
+            {
+                Console.WriteLine($"{day} is not a 'day of the week' number.");
+                Console.WriteLine("Please, type in another value:");
+                goto Start_B;
+            }
+            else
+            {
+                // Здесь не совсем магическое число: в классической 7-ми дневной неделе как правило 2 выходных дня. Поэтому отнимаем от общей длины массива два дня.
+                if(week_num <= 5)
+                {
+                    Console.Write($"{wochentage[week_num - 1]}({day}) in not a weekend day, it's a business day. Go back to work!");
+                }
+                else
+                {
+                    Console.Write($"{wochentage[week_num - 1]}({day}) is a weekend day. Have some rest.");
+                }
+            }
+        }
     }
 }
